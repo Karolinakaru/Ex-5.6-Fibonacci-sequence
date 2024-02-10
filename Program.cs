495044f4a5fa4b347b37e52d06b4a220a4480dba
singhet <singhet@SCSWL4072>
2024-02-10 22:28:00 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class Program
+{
+    private static int Fibonacci(int n)
+    {
+        if (n == 0) return 0;
+
+        else if (n == 1) return 1;
+
+        else if (n < 10) return Fibonacci(n - 1) + Fibonacci(n - 2);
+
+        else return 0; // dla n >= 10
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("First 10 numbers of the Fibonacci sequence:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
